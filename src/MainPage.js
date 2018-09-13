babe9ab2282205adcae3c4fe1f9a06bfe0aba2c8
beckybair <beckyhowen@gmail.com>
2018-09-13 18:12:42 -0500

    added new components

---
+++ src/MainPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Bookshelf from './Bookshelf';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <Bookshelf
+            books={this.props.books}
+            moveShelf={this.props.moveShelf}
+          />
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
